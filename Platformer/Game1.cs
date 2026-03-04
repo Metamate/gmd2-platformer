a05b5c3606eecbe0288417ddfcdb916ea9830134
Metamate <jakikob@gmail.com>
2026-03-04 09:10:16 +0100

    first commit

---
+++ Platformer/Game1.cs
@@ -0,0 +1,47 @@
+﻿using GMDCore;
+using GMDCore.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Platformer;
+
+public class Game1 : Core
+{
+    public const int VirtualWidth = 320;
+    public const int VirtualHeight = 180;
+    private InputHandler _inputHandler;
+    private Tilemap _tilemap;
+
+    public Game1() : base("Platformer", 1280, 720, VirtualWidth, VirtualHeight)
+    {
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        TextureAtlas atlas = TextureAtlas.FromFile(Content, "images/atlas-definition.xml");
+        _tilemap = Tilemap.FromFile(Content, "images/tilemap-definition.xml");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        SpriteBatch.Begin(transformMatrix: ScreenScaleMatrix, samplerState: SamplerState.PointClamp);
+
+
+
+        SpriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
---
+++ Platformer/InputHandler.cs
@@ -0,0 +1,13 @@
+using GMDCore;
+using Microsoft.Xna.Framework.Input;
+
+namespace Platformer;
+
+public class InputHandler
+{
+
+    public void HandleInput()
+    {
+
+    }
+}
